106c8f3083cc753ce5ab72b1ab1163406bc078a5
lkarjun <lk.arjun@hotmail.com>
2020-06-05 15:41:41 +0530

    CreateFile user defined function Introduced.

--- fileReport.py
+++ fileReport.py
@@ -1,1 +1,12 @@
 
+def createFile(seatallocating, classroom, rows):
+    fileName = classroom+'.txt'
+    with open(fileName, 'w') as file:
+        file.write(f'Classroom:  {classroom.upper()}')
+        for row in range(rows):
+            file.write(f'\n\nRow: {row+1}')
+            file.write('\n\n')
+            for key, value in seatallocating[row].items():
+                file.write(f'{key}. {value.upper()}      ')
+    
+    print('Ok.................................')
